1fe9ab7f084c345d3d69874c4d9f8d4d218c1c6a
atviorka <120952488+atviorka@users.noreply.github.com>
2025-10-20 18:50:56 +0100

    tweaked context prompt for AI

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ function generatePoem(event) {
 
     let apiKey = "fb2eo2345t09c60c3a4b1c043a90eee3";
     let prompt = `User instructions: generate a French poem about ${instructionsInput}`;
-    let context = "You are a romantic Poem expert and love to write short poems. Your mission is to generate a four line poem, please add an HTML <br> line break after each line. Make sure to follow the user instructions and do not display anythign extra - only the poem itself."
+    let context = "You are a romantic Poem expert and love to write short poems. Your mission is in HTML format, generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     console.log("generating poem");
