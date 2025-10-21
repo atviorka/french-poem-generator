b8fb8a327e2609cd57ef730133d6c8988618c4dd
atviorka <120952488+atviorka@users.noreply.github.com>
2025-10-21 17:05:54 +0100

    amended context variable & changed some css styling

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ function generatePoem(event) {
 
     let apiKey = "fb2eo2345t09c60c3a4b1c043a90eee3";
     let prompt = `User instructions: generate a funny rapper name based on submitted favourite colour & snack in ${instructionsInput}`;
-    let context = "You are a hilarious rapper name generator. Please ensure the rap name is based on what's being submitted in the form via ${instructionsInput}. Your mission is to write the funny rap name in HTML format (do not include markdown in your response) and add a dollar sign for each letter 's' instead (only in the rap name), inside a <strong> element. Please add a <br/> after the rap name and add a funny 4 line rap verse. Sign the rap name & verse with 'SheCodes AI' inside a <strong> element at the end of the rap name and NOT at the beginning";
+    let context = "You are a hilarious rapper name generator. Please ensure the rap name is based on what's being submitted in the form via ${instructionsInput}. Your mission is to write the funny rap name in HTML format (do not include markdown in your response) and add a dollar sign for each letter 's' instead (only in the rap name), inside a <h2> element. Please add a <br/> after the rap name and add a funny 4 line rap verse. Add 2 x <br/> after the verse and sign it with 'SheCodes AI' inside a <strong> element - at the end and NOT at the beginning.";
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     console.log("generating poem");
