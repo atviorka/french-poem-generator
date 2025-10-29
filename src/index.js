466d66558180fb7091e2f8d136e117163373972d
atviorka <120952488+atviorka@users.noreply.github.com>
2025-10-29 16:08:44 +0000

    separated prompt and the context more
    made sure AI works as intended

--- src/index.js
+++ src/index.js
@@ -12,10 +12,12 @@ function generateRapName(event) {
     event.preventDefault();
 
     let instructionsInput = document.querySelector("#user-instructions");
+    console.log(instructionsInput);
+    console.log(instructionsInput.value);
 
     let apiKey = "fb2eo2345t09c60c3a4b1c043a90eee3";
-    let prompt = `User instructions: generate a funny rapper name based on submitted favourite colour & snack in ${instructionsInput}`;
-    let context = "You are a hilarious rapper name generator. Please ensure the rap name is based on what's being submitted in the form via ${instructionsInput}. Your mission is to write the funny rap name in HTML format (do not include markdown in your response) and add a dollar sign for each letter 's' instead (only in the rap name), inside a <h2> element. Please add a <br/> after the rap name and add a funny 4 line rap verse. Add 2 x <br/> after the verse and sign it with 'SheCodes AI' inside a <strong> element - at the end and NOT at the beginning.";
+    let prompt = `My favourites are ${instructionsInput.value}`;
+    let context = "Generate a funny rapper name based on the favourites. You are a hilarious rapper name generator. Your mission is to write the funny rap name and add a dollar sign for each letter 's' instead (only in the rap name), inside a <h2> element. Please add a <br/> after the rap name add a funny 4 line rap verse related to the name. Add 2 x <br/> after the verse and sign it with 'SheCodes AI' inside a <strong> element - at the end and NOT at the beginning.";
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     let rapNameElement = document.querySelector("#rapName");
